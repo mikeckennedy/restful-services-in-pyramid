d8b0de4ad13cc4a42a84574079b877ed8b18d1e6
Michael Kennedy <mikeckennedy@gmail.com>
2017-05-03 14:48:57 -0700

    Adds service implementation for /api/autos and /api/autos/{car_id}.

--- src/svc1_created_first_auto_service/svc1_first_auto_service/__init__.py
+++ src/svc1_created_first_auto_service/svc1_first_auto_service/__init__.py
@@ -6,18 +6,7 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
-
-    register_routes(config)
-
-    return config.make_wsgi_app()
-
-
-def register_routes(config):
-    config.add_static_view('static', 'static', cache_max_age=1)
-
+    config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-
-    config.add_route('autos_api', '/api/autos')
-    config.add_route('auto_api', '/api/autos/{car_id}')
-
     config.scan()
+    return config.make_wsgi_app()
