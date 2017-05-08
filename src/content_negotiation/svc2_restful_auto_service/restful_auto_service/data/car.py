c191fe472fe1e22e82700340082f8612dec4c350
Michael Kennedy <mikeckennedy@gmail.com>
2017-05-08 11:37:26 -0700

    Separate section for content types.

---
+++ src/content_negotiation/svc2_restful_auto_service/restful_auto_service/data/car.py
@@ -0,0 +1,26 @@
+class Car:
+    def __init__(self, brand, name, price, year, damage, last_seen, image, id=None):
+        self.image = image
+        self.last_seen = last_seen
+        self.damage = damage
+        self.year = year
+        self.price = price
+        self.name = name
+        self.brand = brand
+        self.id = id
+
+    @staticmethod
+    def from_dict(data_dict):
+        return Car(**data_dict)
+
+    def to_dict(self):
+        return {
+            'brand': self.brand,
+            'name': self.name,
+            'price': self.price,
+            'year': self.year,
+            'damage': self.damage,
+            'last_seen': self.last_seen,
+            'id': self.id,
+            'image': self.image
+        }
