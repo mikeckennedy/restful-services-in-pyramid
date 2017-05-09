fe6ca49bfe375fe5025104d6d76255e464568dbd
Michael Kennedy <mikeckennedy@gmail.com>
2017-05-09 14:30:30 -0700

    Before content negotiation, after multiple custom renderers.

---
+++ src/content_negotiation/pre_negotiation_svc3_restful_auto_service/restful_auto_service/renderers/json_renderer.py
@@ -0,0 +1,10 @@
+from pyramid.renderers import JSON
+
+from restful_auto_service.renderers.abstract_renderer import RendererAbstractBase
+
+
+class JSONRendererFactory(JSON, RendererAbstractBase):
+
+    def can_serialize_value(self, value):
+        return True  # how do we know?
+
